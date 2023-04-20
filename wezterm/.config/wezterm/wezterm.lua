fbb255aa96b74c0f3b4a625a5d5aa6a7477b59e8
pantz8622 <pantz8622@gmail.com>
2023-04-20 13:44:54 +0800

    style: change default font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local colors = wezterm.color.load_scheme("/home/wadeyao/.config/wezterm/assets/colors/tokyonight_storm.toml")
 
 return {
-    font = wezterm.font 'LXGW WenKai Mono Light',
+    font = wezterm.font 'Xiaolai Mono SC',
     font_size = 12,
     line_height = 1.3,
     enable_tab_bar = false,
