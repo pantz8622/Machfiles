4aa4ea1097e0f69f99b1bd2aa7dc18b034fd028f
Wade yao <pantz8622@gmail.com>
2024-10-14 00:49:56 +0800

    feat: navigation optimization

--- nvim/.config/nvim/lua/pantz8622/nvim-tree.lua
+++ nvim/.config/nvim/lua/pantz8622/nvim-tree.lua
@@ -1,16 +1,70 @@
 local nvim_tree = require("nvim-tree")
 nvim_tree.api = require("nvim-tree.api")
 
-local function open()
-  nvim_tree.api.node.open.edit()
-  nvim_tree.api.tree.focus()
+-- Navigation --
+
+local navigate_history = {}
+
+local function navigate_up()
+  local node = nvim_tree.api.tree.get_node_under_cursor()
+  if node.name ~= ".." then
+    table.insert(navigate_history, vim.api.nvim_win_get_cursor(0)[1])
+    nvim_tree.api.node.navigate.parent()
+  end
 end
 
-local function open_quit()
-  nvim_tree.api.node.open.edit()
+local function navigate_down()
+  if #navigate_history ~= 0 then
+    local lnr = table.remove(navigate_history, #navigate_history)
+    vim.cmd(string.format("norm! %dG", lnr))
+  end
+end
+
+local function navigate_next()
+  navigate_history = {}
+  vim.cmd("norm! j")
+end
+
+local function navigate_prev()
+  navigate_history = {}
+  vim.cmd("norm! k")
+end
+
+local function navigate_sibling_next()
+  navigate_history = {}
+  nvim_tree.api.node.navigate.sibling.next()
+end
+
+local function navigate_sibling_prev()
+  navigate_history = {}
+  nvim_tree.api.node.navigate.sibling.prev()
+end
+
+-- Root Navigation -- 
+
+local history = {}
+
+local function edit_goto()
   local node = nvim_tree.api.tree.get_node_under_cursor()
-  if node.nodes == nil and node.name ~= '..' then -- quit only if it's a file
-    nvim_tree.api.tree.close()
+  if node.nodes == nil and node.name ~= ".." then -- open and quit if it's a file
+    nvim_tree.api.node.open.edit()
+  else
+    table.insert(history, vim.fn.getcwd())
+    nvim_tree.api.tree.change_root_to_node()
+  end
+end
+
+local function backto_prev()
+  if #history ~= 0 then
+    nvim_tree.api.tree.change_root(table.remove(history, #history))
+  end
+end
+
+local function open()
+  local node = nvim_tree.api.tree.get_node_under_cursor()
+  if node.name ~= ".." then
+    nvim_tree.api.node.open.edit()
+    nvim_tree.api.tree.focus()
   end
 end
 
@@ -22,25 +76,37 @@ local function on_attach(bufnr)
   -- default mappings
   -- nvim_tree.api.config.mappings.default_on_attach(bufnr)
 
+  -- navigate
+  vim.keymap.set('n', '<CR>',          edit_goto,                                       opts('Edit & Goto'))
+  vim.keymap.set('n', '<S-CR>',        backto_prev,                                     opts('Goto Previous'))
+  vim.keymap.set('n', 'h',             navigate_up,                                     opts('Up'))
+  vim.keymap.set('n', 'l',             navigate_down,                                   opts('Down'))
+  vim.keymap.set('n', 'j',             navigate_next,                                   opts('Next'))
+  vim.keymap.set('n', 'k',             navigate_prev,                                   opts('Prev'))
+  vim.keymap.set('n', '<S-j>',         navigate_sibling_next,                           opts('Next Sibling'))
+  vim.keymap.set('n', '<S-k>',         navigate_sibling_prev,                           opts('Prev Sibling'))
   vim.keymap.set('n', 'w',             nvim_tree.api.tree.collapse_all,                 opts('Collapse'))
   vim.keymap.set('n', 'e',             nvim_tree.api.tree.expand_all,                   opts('Expand All'))
-  vim.keymap.set('n', 'K',             nvim_tree.api.node.show_info_popup,              opts('Info'))
-  vim.keymap.set('n', 'y',             nvim_tree.api.fs.copy.filename,                  opts('Copy Name'))
+
+  -- file edit
+  vim.keymap.set('n', 'n',             nvim_tree.api.fs.create,                         opts('Create'))
+  vim.keymap.set('n', 'd',             nvim_tree.api.fs.trash,                          opts('Trash'))
+  vim.keymap.set('n', 'r',             nvim_tree.api.fs.rename,                         opts('Rename'))
   vim.keymap.set('n', '<C-c>',         nvim_tree.api.fs.copy.node,                      opts('Copy'))
   vim.keymap.set('n', '<C-x>',         nvim_tree.api.fs.cut,                            opts('Cut'))
   vim.keymap.set('n', '<C-v>',         nvim_tree.api.fs.paste,                          opts('Paste'))
-  vim.keymap.set('n', '<C-f>',         nvim_tree.api.live_filter.start,                 opts('Filter'))
+
+  -- others
+  vim.keymap.set('n', '<Space>',       open,                                            opts('Open'))
+  vim.keymap.set('n', '<2-LeftMouse>', open,                                            opts('Open'))
+  vim.keymap.set('n', 'i',             nvim_tree.api.node.show_info_popup,              opts('Info'))
+  vim.keymap.set('n', 'y',             nvim_tree.api.fs.copy.filename,                  opts('Copy Name'))
+  vim.keymap.set('n', '/',             nvim_tree.api.live_filter.start,                 opts('Filter'))
   vim.keymap.set('n', '<Esc>',         nvim_tree.api.live_filter.clear,                 opts('Clean Filter'))
-  vim.keymap.set('n', 'd',             nvim_tree.api.fs.trash,                          opts('Trash'))
-  vim.keymap.set('n', 'n',             nvim_tree.api.fs.create,                         opts('Create'))
-  vim.keymap.set('n', 'r',             nvim_tree.api.fs.rename,                         opts('Rename'))
-  vim.keymap.set('n', '<CR>',          open_quit,                                       opts('open & quit'))
-  vim.keymap.set('n', '<S-CR>',        nvim_tree.api.node.navigate.parent,              opts('Parent Directory'))
-  vim.keymap.set('n', '<Space>',       open,                                            opts('open'))
-  vim.keymap.set('n', '<2-LeftMouse>', open,                                            opts('open'))
-  vim.keymap.set('n', 'h',             nvim_tree.api.tree.toggle_help,                  opts('Help'))
+  vim.keymap.set('n', '?',             nvim_tree.api.tree.toggle_help,                  opts('Help'))
 end
 
+-- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -71,15 +137,16 @@ nvim_tree.setup {
           staged = "",
           unmerged = "",
           renamed = "",
-          untracked = "◌",
+          untracked = "",
           deleted = "",
           ignored = "",
         },
       },
+      git_placement = "signcolumn",
     },
   },
   git = {
-    enable = false,
+    enable = true,
   },
   diagnostics = {
     enable = true,
@@ -98,6 +165,7 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
+    signcolumn = "yes",
   },
 }
 
